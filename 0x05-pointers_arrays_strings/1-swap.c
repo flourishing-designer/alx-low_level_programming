cbe1dbd24df4d653df255a5e180669191850c768
flourishing-designer <phunmeton4@gmail.com>
2022-10-24 18:08:41 -0700

    Task 1 update

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -2,13 +2,15 @@
 
 /**
  * swap_int - Swaps the values of 2 integers.
- * @a: first arguement to be swapped.
- * @b: second arguement to be swapped.
- * Return: void
+ * @a: int a
+ * @b: int b
  */
+
 void swap_int(int *a, int *b)
 {
-	int p = *a;
+	int q;
+
+	q = *a;
 	*a = *b;
-	*b = p;
+	*b = q;
 }
